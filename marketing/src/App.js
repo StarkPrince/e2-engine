823f6891c691489b5cf3b8281a15744e7882ea3f
Starkprince <games.princeraj@gmail.com>
2023-03-14 03:41:09 +0530

    day 1 of ecom2

---
+++ marketing/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { StyleProvider } from "@material-ui/core/styles";
+
+import Landing from "./components/Landing";
+import Pricing from "./components/Pricing";
+
+export default () =>
+{
+    return (
+        <div>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/pricing" component={Pricing} />
+                    <Route path="/" component={Landing} />
+                </Switch>
+            </BrowserRouter>
+        </div>
+    );
+};
